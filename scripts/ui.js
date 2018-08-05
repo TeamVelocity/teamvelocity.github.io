5c5783a9a4429368fc1a53672556046321cfc28b
bmweiner <bmweiner@users.noreply.github.com>
2018-08-05 17:30:52 -0400

    Updated round and player classes for gameplay.

--- scripts/ui.js
+++ scripts/ui.js
@@ -11,18 +11,8 @@ $(document).ready(function(){
     // hide setup view
     $(".setup").hide();
 
-    game = new Game();
-
-    // create default boards
-    let data = [data1, data2];
-
-    for(let i=0; i<data.length; i++){
-        let round = game.addRound();
-
-        round.board.import(JSON.stringify(data[i]));
-        round.wheel.assignSectors();
-        round.wheel.randomizeSectors();
-    }
+    // init default game
+    game = initDefaultGame();
 
     // edit board content if needed
     round = game.getRound(0);
@@ -30,31 +20,61 @@ $(document).ready(function(){
     clue.question = "What is the oldest soft drink in America?";
     clue.answer = "Dr. Pepper.";
 
+    // setup players
+    // $("#btn-play").click(function(){
+    //     // store players -- should be adjusted for num of players and default names
+    //     let playerName = $("#player0").find("input").val();
+    //     game.addPlayer(playerName);
+    // })
 
-    // start game
-    $("#btn-play").click(function(){
-        // store players -- should be adjusted for num of players and default names
-        let playerName = $("#player0").find("input").val();
-        game.addPlayer(playerName);
-    })
+    // add sample players for testing
+    game.addPlayer("John");
+    game.addPlayer("Jane");
 
     // start first round
     round = game.getRound(0);
     round.start();
 
-    // player spins
-    console.log(round.currentPlayer);
-    let spin = round.wheel.spin();
-
-    // animate the wheel
-
-    // display outcome
-    console.log(spin);
-
-    // address outcome
-
-    // check if round is over
-    if(round.complete){
+    if(game.complete){
+        // end game
+    } else if (round.complete){
         // end round
+    } else {
+        // prepare UI for first spin
+
+        // spin wheel
+        //let spin = round.spin();
+
+        // animate UI
+
+        // respond to outcome
+
+        // if category and not spin again, display the clue
+        //clue = spin.clue;
+
+        // if player or opponents choice
+        // let categoryColumn = 0;
+        // let clue = round.pickCategory(categoryColumn);
+
+        // if answer is valid
+        // round.validAnswer();
+        
+        // if answer is invalid
+        // round.invalidAnswer()
+
+        // if lose turn or wrong answer and player has a token
+        // give option to use token
+        // if (round.stats.currentPlayer.hasToken){
+        //     round.useToken();
+        //     // start over at spin
+        // } else {
+        //     // do not allow token use
+        // }
+
+
+        // update players score and tokens
+
+        // reset for next spin
+        // round.endTurn();
     }
   });

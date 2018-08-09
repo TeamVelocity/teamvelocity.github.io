5284ff49f198e0dd4be5cf6219e6cc7e8487fd23
ngray10 <nathaniel.gray@gmail.com>
2018-08-09 02:33:22 -0400

    [exper] data API integration; style changes
    Integrated the experimental index with the API; migrated exper.js into ui.js. Added select handler while in edit mode for changing rounds. Data populates the table at page load.
    Updated the table's CSS to reflect Jeopardy colors - removed hover color changes - added special border for headers.
    
    TODO: persist edits back into the API (not implemented)
    TODO: Need to have static table size, it resizes when data loads

--- scripts/ui.js
+++ scripts/ui.js
@@ -11,6 +11,125 @@ let playerCount = 3;
 let category_editable = false;
 let trivia_editable = false;
 
+// keep track of some elements of the table that have been manipulated by the user
+let fill_value = "";
+let clicked_col;
+let clicked_row;
+let clicked_element;
+let clicked_trivia_index = 0;
+
+let category_form;
+let trivia_form;
+let category_dialog;
+let trivia_dialog;
+let frm_name_category = $('input[name="frm_name_category"]');
+let frm_name_trivia_question = $('input[name="frm_name_trivia_question"]');
+let frm_name_trivia_answer = $('input[name="frm_name_trivia_answer"]');
+let tips = $( ".validateTips" );
+
+// set up the edit category dialog
+category_dialog = $( "#category-form" ).dialog({
+    autoOpen: false,
+    height: 400,
+    width: 350,
+    modal: true,
+    buttons: {
+        "Edit Category": editCategory,
+        Cancel: function() { 
+            category_dialog.dialog( "close" );
+        }
+    },
+    close: function() { frm_name_category.removeClass( "ui-state-error" ); }
+});
+
+// setup the edit trivia dialog
+trivia_dialog = $( "#trivia-form" ).dialog({
+    autoOpen: false,
+    height: 400,
+    width: 350,
+    modal: true,
+    buttons: {
+        "Edit Trivia": editTrivia,
+        Cancel: function() { 
+            trivia_dialog.dialog( "close" );
+        }
+    },
+    close: function() { 
+        frm_name_trivia_question.removeClass( "ui-state-error" );
+        frm_name_trivia_answer.removeClass( "ui-state-error" );
+    }
+});    
+
+// set up the form to edit the category text
+category_form = category_dialog.find( "form" ).on( "submit", function( submit_event ) {
+    submit_event.preventDefault();
+    frm_name_category.val("");
+    });
+
+    // set up the form to edit the trivia text
+trivia_form = trivia_dialog.find( "form" ).on( "submit", function( submit_event ) {
+    submit_event.preventDefault();
+});
+
+// function used to update the trivia modal dialog
+function populateTriviaFromEngine(clicked_col, clicked_row) {
+    
+    let roundID = 0;
+    let round;
+
+    // the update given -1 will read from the UI widget -- e.g. editing mode
+    if (gameRound == -1) {
+        // get the round chosen from the round drop down box
+        roundID = $("#select-round").val() - 1;
+        round = game.getRound(roundID);
+    }
+
+    round = game.getRound(gameRound);
+    // fetch the clue from the engine
+    cell_content = round.board.getClue(clicked_col, clicked_row);
+
+}
+
+function initGameBoardFromRound(roundChoice) {
+
+    console.log(roundChoice);
+    let edit_round = game.getRound(roundChoice);   
+    
+    // setup the points (cells) for the game as chosen in the drop down box
+    $("#triviaTable tbody tr").each(function (row_index, row) {
+        let current_row = $(row);
+        console.log("Row Index: " + row_index);
+        /*
+        + ", row: ");
+        console.log(current_row);*/
+        let cols = 6;
+        let column_index = 0;
+        let current_clue;
+        for (column_index = 0; column_index < cols; column_index++) {
+            let current_column = current_row[0].cells[column_index];
+            console.log("Column Index: " + column_index + ", column text: " + current_column.innerHTML);
+            current_clue = edit_round.board.getClue(column_index, row_index);
+            current_column.innerHTML = current_clue.points;
+        }
+    });
+
+    // setup the categories
+    let current_categories = edit_round.board.categoryNames;
+    $("#triviaTable thead th").each(function (category_index, category) {
+        let current_category_cell = $(category);
+        console.log("category Index: " + category_index);
+        console.log("category name: " + current_categories[category_index]);
+        console.log($(category));
+        $(category)[0].innerHTML = current_categories[category_index];
+    });
+}
+
+function initGameBoard() {
+
+    // setup the game board assuming round 0
+    initGameBoardFromRound(0);
+}
+
 function download(filename, text) {
     // source: https://goo.gl/VWW2sT
     var element = document.createElement('a');
@@ -66,15 +185,18 @@ function startEdit(){
     toggleTriviaClicks();
 }
 
-function endEdit(){
+function endEdit() {
     $(".edit-panel").hide();
     $(".start-panel").show();
 
     toggleCategoryClicks();
     toggleTriviaClicks();
+
+    // get the round chosen from the round drop down box
+    // populateTriviaFromEngine(0);
 }
 
-function startGame(){
+function startGame() {
     $(".start-panel").hide();
     $(".play-panel").show();
 
@@ -83,7 +205,7 @@ function startGame(){
     }
 }
 
-function removePlayer(){
+function removePlayer() {
     if(playerCount > 1){
         playerCount--;
         $("#player" + playerCount).hide();
@@ -93,7 +215,7 @@ function removePlayer(){
     }
 }
 
-function addPlayer(){
+function addPlayer() {
     if(playerCount < 3){
         $("#player" + playerCount).show();
         playerCount++;
@@ -116,9 +238,73 @@ function toggleCategoryClicks() {
 function toggleTriviaClicks() {
     if ( trivia_editable ){ trivia_editable = false; }
     else { trivia_editable = true; }
-}   
+}
 
-$(document).ready(function(){
+function editCategory() {
+    let valid = true;
+    frm_name_category.removeClass( "ui-state-error" );
+    valid = valid && checkLength( frm_name_category, "category", 3, 16 );
+    valid = valid && checkRegexp( frm_name_category, /^[a-z]([0-9a-z_\s])+$/i, "Input may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
+
+    // validates the form's input field, and closes the modal
+    if ( valid ) {
+        clicked_element.target.innerHTML = frm_name_category.val();
+        category_dialog.dialog( "close" );
+    }
+    return valid;
+}
+
+function editTrivia() {
+    let valid = true;
+    frm_name_trivia_question.removeClass( "ui-state-error" );
+    frm_name_trivia_answer.removeClass( "ui-state-error" );
+
+    valid = valid && checkLength( frm_name_trivia_question, "trivia", 3, 256 );
+    valid = valid && checkRegexp( frm_name_trivia_question, /^[a-z]([0-9a-z_\s])+$/i, "Input may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );
+    valid = valid && checkLength( frm_name_trivia_answer, "trivia", 3, 256 );
+    valid = valid && checkRegexp( frm_name_trivia_answer, /^[a-z]([0-9a-z_\s])+$/i, "Input may consist of a-z, 0-9, underscores, spaces and must begin with a letter." );        
+
+    // validates the form's input field, and closes the modal
+    if ( valid ) {
+        // store the user's trivia information in the local session table
+        questions[clicked_trivia_index] = frm_name_trivia_question.val();
+        answers[clicked_trivia_index] = frm_name_trivia_answer.val();
+
+        console.log("Input: \nQuestion: " + questions[clicked_trivia_index] + "\nAnswer: " + answers[clicked_trivia_index]);
+
+        trivia_dialog.dialog( "close" );
+    }
+    return valid;
+}
+
+function updateTips( t ) {
+    tips
+        .text( t )
+        .addClass( "ui-state-highlight" );
+    setTimeout(function() {
+        tips.removeClass( "ui-state-highlight", 1500 );
+    }, 500 );
+}
+
+function checkLength( o, n, min, max ) {
+    if ( o.val().length > max || o.val().length < min ) {
+        o.addClass( "ui-state-error" );
+        updateTips( "Length of " + n + " must be between " + min + " and " + max + "." );
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function checkRegexp( o, regexp, n ) {
+    if ( !( regexp.test( o.val() ) ) ) {
+        o.addClass( "ui-state-error" );
+        updateTips( n );
+        return false;
+    } else { return true; }
+}
+
+$(document).ready(function() {
     // hide panels
     $(".edit-panel").hide();
     $(".play-panel").hide();
@@ -155,11 +341,69 @@ $(document).ready(function(){
         console.log(playerCount)        
     });
 
+    // setup the selection handler for the select ddl
+    $("#select-round").change(function( clicked_object ) {
+        let round_selected = $(this).children(":selected").html();
+        console.log("Round changed! " + round_selected);
+        let selected_round = parseInt( round_selected) - 1;
+        initGameBoardFromRound(selected_round);
+    });
+
+    // set up the click event for the trivia elements
+    $("#triviaTable td").click(function( clicked_object ) {
+
+        clicked_col = parseInt( $(this).index());
+        clicked_row = parseInt( $(this).parent().index());        
+
+        clicked_element = clicked_object;
+        if ( trivia_editable ) {
+            // ensure the form is populated with the current value of the table element before open
+            // trivia will extract the current value from the stored (local storage) values depending on the td value read
+
+            let output = "Trivia: clicked_row = " + clicked_row + ",  clicked_col = " + clicked_col;
+            console.log(output);
+
+            // get the game round if necessary
+            if (typeof round == 'undefined') { 
+                let roundID = $("#select-round").val() - 1;
+                round = game.getRound(roundID);
+            }
+
+            // get the q & a for the current round
+            let clue = round.board.getClue(clicked_col, clicked_row);            
+            let question = clue.question;
+            let answer = clue.answer;
+            
+            if (typeof question == 'undefined'){ question = "<empty>"; }           
+            if (typeof answer == 'undefined'){ answer = "<empty>"; }
+
+            output = "Clue: Question: " + " | " + question + "\nAnswer: " + answer;
+            console.log(output);
+            
+            // pre-populate the trivia information before modal open
+            frm_name_trivia_question.val(question);
+            frm_name_trivia_answer.val(answer);
+
+            trivia_dialog.dialog( "open" );
+        }
+    });
+
+    // set up the click event for the category elements
+    $("#triviaTable th").click(function( clicked_object ) {
+        clicked_element = clicked_object;
+        if ( category_editable ) {
+            // ensure the form is populated with the current value of the table element before open
+            frm_name_category.val($(this).text());
+            category_dialog.dialog( "open" );
+        }
+    });    
+
     // init default game
     game = initDefaultGame();
-
-    // edit board content if needed
     round = game.getRound(0);
+    // load the game data for the first round
+    initGameBoard();
+
     let clue = round.board.getCategory(1).clues[2];
     clue.question = "What is the oldest soft drink in America?";
     clue.answer = "Dr. Pepper.";
